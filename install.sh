5092db649634f0e3d54509b6005215f93bec4930
Enes Özeser <54821628+enesozeser@users.noreply.github.com>
2020-09-28 18:42:47 +0300

    Quench's Installer

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 mv quench.py quench
 chmod a+x quench
-cp quench /usr/bin/
+cp quench /usr/local/bin/
 mv quench quench.py
 apt install python3 -y
 sleep 5
-printf "${YELLOW}Quench has been installed. Use '${GREEN}quench -h${YELLOW}' command for all information.\n"
+printf "${YELLOW}Quench has been installed. Use '${GREEN}quench${YELLOW}' command for all information.\n"
 printf "${NC}"
