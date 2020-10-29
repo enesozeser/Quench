c14c1a7c020eebe2fd81e5cf2501604eac40feaf
Enes Özeser <54821628+enesozeser@users.noreply.github.com>
2020-10-29 18:13:54 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mv quench.py quench
-chmod a+x quench
-cp quench /usr/local/bin/
-mv quench quench.py
-apt update && apt install python3 -y
-sleep 2
-echo "Quench has been installed. Use 'quench' command for all information."
